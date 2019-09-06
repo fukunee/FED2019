2eb34f1a539021a48abfad6866a0f08762b6896c
fukun <fukun.ee@foxmail.com>
2019-09-07 01:37:28 +0800

    写了lc_003并更新了一些心得

---
+++ lc/solutions/lc_003.js
@@ -0,0 +1,15 @@
+let lengthOfLongestSubstring = function (str) {
+    let m = 0;
+    let _arr = [];
+    for (let i = 0; i < str.length; i++) {
+        let idx = _arr.indexOf(str[i]);
+        _arr.push(str[i]);
+        if (idx >= 0) {
+            _arr = _arr.splice(idx + 1);
+        }
+        m = Math.max(m, _arr.length);
+    }
+    return m;
+};
+
+module.exports = lengthOfLongestSubstring;
---
+++ lc/tests/lc_003.test.js
@@ -0,0 +1,5 @@
+let lengthOfLongestSubstring = require('../solutions/lc_003');
+
+test('lc_003 unit test', () => {
+    expect(lengthOfLongestSubstring("abcabcbb")).toStrictEqual(3);
+});
