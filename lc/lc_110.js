972a8a43a6ef7cfe46c3669b279b3c839e00be2f
fukun <fukun.ee@foxmail.com>
2019-09-23 18:43:10 +0800

    Update lc_110.js

--- lc/lc_110.js
+++ lc/lc_110.js
@@ -3,22 +3,20 @@ function TreeNode(val) {
     this.left = this.right = null;
 }
 
-
-const isBalanced = function (root) {
-    const calHeight = function (node, tmp) {
-        if (!node) {
-            depth.push(tmp);
-            return Math.max.apply(Math, depth) - Math.min.apply(Math, depth) <= 1;
-        }
-        return calDepth(node.left, tmp + 1) && calDepth(node.right, tmp + 1);
+var isBalanced = function (root) {
+    const calHeight = function (node) {
+        if (!node) return 0;
+        let ll = calHeight(node.left),
+            lr = calHeight(node.right);
+        if (ll === false || lr === false) return false;
+        if (Math.abs(ll - lr) >= 2) return false;
+        return Math.max(ll, lr) + 1;
     };
-    return calDepth(root, 0);
+    return calHeight(root) !== false;
 };
- 
+
+
 let t = new TreeNode(1);
 t.left = new TreeNode(2);
 t.right = new TreeNode(2);
-t.left.left = new TreeNode(3);
-// t.left.left.left = new TreeNode(3);
-
 console.log(isBalanced(t));
