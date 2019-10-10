389ec38ec73303fc76f36d8380016a7422ab2203
fukunee <fukun.ee@foxmail.com>
2019-10-10 17:20:01 +0800

    cm
    cm

--- lc/lc_121.js
+++ lc/lc_121.js
@@ -1,11 +1,11 @@
 const maxProfit = function (prices) {
-    let dp0 = -prices[0];
-    let dp1 = 0;
+    let dp1 = -prices[0];
+    let dp0 = 0;
     for (let i = 0; i < prices.length; i++) {
-        dp0 = Math.max(dp0, -prices[i]);
-        dp1 = Math.max(dp0 + prices[i], dp1);
+        dp1 = Math.max(dp1, -prices[i]);
+        dp0 = Math.max(dp1 + prices[i], dp0);
     }
-    return dp1;
+    return dp0;
 };
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
