2497c28aa29fcc275a6910ea2b3505bb7fea373c
fukunee <fukun.ee@foxmail.com>
2019-10-10 12:33:41 +0800

    CM
    cm

--- lc/lc_121.js
+++ lc/lc_121.js
@@ -1,5 +1,11 @@
-const maxProfit = function(prices) {
-
+const maxProfit = function (prices) {
+    let dp0 = -prices[0];
+    let dp1 = 0;
+    for (let i = 0; i < prices.length; i++) {
+        dp0 = Math.max(dp0, -prices[i]);
+        dp1 = Math.max(dp0 + prices[i], dp1);
+    }
+    return dp1;
 };
 
-console.log(maxProfit([7,1,5,3,6,4]));
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
