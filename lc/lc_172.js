26caec5f2d49ea5f46866453fc167f7c8e461d5d
fukun <fukun.ee@foxmail.com>
2019-10-14 04:26:03 +0800

    Update lc_172.js

--- lc/lc_172.js
+++ lc/lc_172.js
@@ -1,4 +1,9 @@
-var trailingZeroes = function(n) {
-
+const trailingZeroes = function (n) {
+    let output = 0;
+    while (n > 0) {
+        n = Math.floor(n / 5);
+        output += n;
+    }
+    return output;
 };
-console.log(trailingZeroes(3));
+console.log(trailingZeroes(2147483647));
