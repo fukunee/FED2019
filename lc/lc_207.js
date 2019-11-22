8be3c890a039141840acb6f77f2c2aea38e6ef16
fukunee <fukun.ee@foxmail.com>
2019-11-22 15:22:46 +0800

    Update lc_207.js

--- lc/lc_207.js
+++ lc/lc_207.js
@@ -1,21 +1,22 @@
-function ListNode(val){
-    this.val=val;
-    this.next=[];
+function ListNode(val) {
+    this.val = val;
+    this.next = [];
+    this.in = 0;
 }
+
 var canFinish = function (numCourses, prerequisites) {
 //build a listnode
     let tmp = {};
-    output=true;
-    prerequisites.forEach(v=>{
-        let a=[1];
-        let b=[0];
-        if(!tmp[a])tmp[a]=new ListNode(a);
-        if(!tmp[b])tmp[b]=new ListNode(b);
+    output = true;
+    prerequisites.forEach(v => {
+        let a = v[1];
+        let b = v[0];
+        if (!tmp[a]) tmp[a] = new ListNode(a);
+        if (!tmp[b]) tmp[b] = new ListNode(b);
         tmp[b].next.push(tmp[a]);
+        tmp[b].in++;
     })
-    tmp.forEach(v=>{
-        if(v===null)return;
 
-    })
+    return inArr;
 };
 console.log(canFinish(2, [[1, 0], [0, 1]]));
